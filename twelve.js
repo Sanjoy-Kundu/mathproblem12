cd7b312a558725a9dd808ef70d5f4eab0bc85e06
sanjoy <sanjoykundu187@gamil.com>
2022-02-04 00:38:22 -0500

    first commit

---
+++ twelve.js
@@ -0,0 +1,11 @@
+/* ====================================================================
+ একটা অবজেক্ট ডিক্লেয়ার করবে। সেটাতে তিনটা প্রপার্টি থাকবে। এবং কোন একটা প্রোপার্টি এর মান চেইঞ্জ করবা। 
+ ===================================================================*/
+var students = { firstName: "Sanjoy", class: "CSE", roll: 0032, homeTown: "Madaripur" };
+console.log(students);
+
+students.class = "Computer Science Enginnerring";
+console.log(students);
+
+students.firstName = "Sanjoy Kundu";
+console.log(students);
